6fdac5c5a8af2d9f013a50388ee6d68ee7ece2fb
DanielGH3 <danielnazuni@gmail.com>
2024-02-12 18:04:13 +0200

    Added textarea line counter

--- main.js
+++ main.js
@@ -1,18 +1,115 @@
 import { CodeConverter } from "./codeconverter.mjs";
 import { Chip8 } from "./chip8.mjs";
 import { ChipScreen } from "./screen.mjs";
-import { CodeScreen } from "./codescreen.mjs";
+import { HexCodeScreen } from "./codescreen.mjs";
 
 const chip = new Chip8();
 
 ChipScreen.init()
 
-chip.load_program('programs/test.ch8').then(() => {
+HexCodeScreen.init(chip, chip.get_memory())
+
+chip.load_program('programs/ibm_logo.ch8').then(() => {
     setInterval(() => {
         for(let i = 0; i < 100; i++) {
             chip.run_instruction()
         }
     }, 100);
 
-    CodeScreen.loadHexCode(chip.get_memory())
+    HexCodeScreen.loadHexCode()
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    const textarea = document.getElementById('textarea');
+    const lineNumbersEle = document.getElementById('line-numbers');
+
+    const textareaStyles = window.getComputedStyle(textarea);
+    [
+        'fontFamily',
+        'fontSize',
+        'fontWeight',
+        'letterSpacing',
+        'lineHeight',
+        'padding',
+    ].forEach((property) => {
+        lineNumbersEle.style[property] = textareaStyles[property];
+    });
+
+    const parseValue = (v) => v.endsWith('px') ? parseInt(v.slice(0, -2), 10) : 0;
+
+    const font = `${textareaStyles.fontSize} ${textareaStyles.fontFamily}`;
+    const paddingLeft = parseValue(textareaStyles.paddingLeft);
+    const paddingRight = parseValue(textareaStyles.paddingRight);
+
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    context.font = font;
+
+    const calculateNumLines = (str) => {
+        const textareaWidth = textarea.getBoundingClientRect().width - paddingLeft - paddingRight;
+        const words = str.split(' ');
+        let lineCount = 0;
+        let currentLine = '';
+        for (let i = 0; i < words.length; i++) {
+            const wordWidth = context.measureText(words[i] + ' ').width;
+            const lineWidth = context.measureText(currentLine).width;
+
+            if (lineWidth + wordWidth > textareaWidth) {
+                lineCount++;
+                currentLine = words[i] + ' ';
+            } else {
+                currentLine += words[i] + ' ';
+            }
+        }
+
+        if (currentLine.trim() !== '') {
+            lineCount++;
+        }
+
+        return lineCount;
+    };
+
+    const calculateLineNumbers = () => {
+        const lines = textarea.value.split('\n');
+        const numLines = lines.map((line) => calculateNumLines(line));
+
+        let lineNumbers = [];
+        let i = 1;
+        while (numLines.length > 0) {
+            const numLinesOfSentence = numLines.shift();
+            lineNumbers.push(i);
+            if (numLinesOfSentence > 1) {
+                Array(numLinesOfSentence - 1)
+                    .fill('')
+                    .forEach((_) => lineNumbers.push(''));
+            }
+            i += 2;
+        }
+
+        return lineNumbers;
+    };
+
+    const displayLineNumbers = () => {
+        const lineNumbers = calculateLineNumbers();
+        lineNumbersEle.innerHTML = Array.from({
+            length: lineNumbers.length
+        }, (_, i) => `<div>${lineNumbers[i] || '&nbsp;'}</div>`).join('');
+    };
+
+    textarea.addEventListener('input', () => {
+        displayLineNumbers();
+    });
+
+    displayLineNumbers();
+
+    const ro = new ResizeObserver(() => {
+        const rect = textarea.getBoundingClientRect();
+        lineNumbersEle.style.height = `${rect.height}px`;
+        displayLineNumbers();
+    });
+    ro.observe(textarea);
+
+    textarea.addEventListener('scroll', () => {
+        lineNumbersEle.scrollTop = textarea.scrollTop;
+    });
+});
